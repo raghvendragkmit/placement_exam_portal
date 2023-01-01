becbd7c1d85e99f3609a7acff7d8b664820f55cb
raghvendragkmit <raghvendra@gkmit.co>
2023-01-01 15:48:35 +0530

    Add get-all-user-serializer

--- serializers/user.serializer.js
+++ serializers/user.serializer.js
@@ -15,6 +15,29 @@ const createUser = async (req, res, next) => {
 	next()
 }
 
+const getAllUser = async (req, res, next) => {
+	const data = res.data || null
+
+	const response = []
+
+	data.forEach((obj) => {
+		const tempObj = {
+			id: obj.id,
+			firstName: obj.first_name,
+			lastName: obj.last_name,
+			email: obj.email,
+			role: obj.role,
+			organization: obj.organization,
+			contactNumber: obj.contactNumber,
+		}
+		response.push(tempObj)
+	})
+
+	res.data = response
+	next()
+}
+
 module.exports = {
 	createUser,
+	getAllUser,
 }
