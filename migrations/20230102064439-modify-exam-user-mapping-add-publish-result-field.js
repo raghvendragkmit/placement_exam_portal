2c6f8493d1eb4af079c26c1168654a4c71ec6076
raghvendragkmit <raghvendra@gkmit.co>
2023-01-02 12:29:58 +0530

    Add migation modify-exam-user-mapping

---
+++ migrations/20230102064439-modify-exam-user-mapping-add-publish-result-field.js
@@ -0,0 +1,22 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		return queryInterface.addColumn(
+			"exam_user_mapping", // table name
+			"publish_result", // new field name
+			{
+				type: Sequelize.BOOLEAN,
+				allowNull: true,
+			}
+		)
+	},
+
+	async down(queryInterface, Sequelize) {
+		return queryInterface.removeColumn(
+			"exam_user_mapping",
+			"publish_result"
+		)
+	},
+}
