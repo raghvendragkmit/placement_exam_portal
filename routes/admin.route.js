8e9038a6dd931cc763b1e5bdce43c585eed0b15e
raghvendragkmit <raghvendra@gkmit.co>
2023-01-02 15:25:51 +0530

    Bugfix delete index.js from controllers, services, serializers,validators

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,25 +1,25 @@
 const { Router } = require("express")
-const controllers = require("../controllers")
+const userController = require("../controllers/user.controller")
 const genericResponse = require("../helpers/common-function.helper")
 const authMiddleware = require("../middlewares/auth")
-const validator = require("../validators/index")
-const serializer = require("../serializers")
+const userValidator = require("../validators/user.validator")
+const userSerializer = require("../serializers/user.serializer")
 const router = Router()
 
 router.post(
 	"/user",
 	authMiddleware.checkAccessToken,
 	authMiddleware.verifyAdmin,
-	validator.userValidator.createUserSchema,
-	controllers.userController.createUser,
-	serializer.userSerializer.createUser,
+	userValidator.createUserSchema,
+	userController.createUser,
+	userSerializer.createUser,
 	genericResponse.sendResponse
 )
 
 router.post(
 	"/login",
-	validator.userValidator.loginSchema,
-	controllers.userController.loginUser,
+	userValidator.loginSchema,
+	userController.loginUser,
 	genericResponse.sendResponse
 )
 
@@ -27,8 +27,8 @@ router.delete(
 	"/user/:userId",
 	authMiddleware.checkAccessToken,
 	authMiddleware.verifyAdmin,
-	validator.userValidator.userIdSchema,
-	controllers.userController.deleteUser,
+	userValidator.userIdSchema,
+	userController.deleteUser,
 	genericResponse.sendResponse
 )
 
@@ -36,30 +36,30 @@ router.get(
 	"/users",
 	authMiddleware.checkAccessToken,
 	authMiddleware.verifyAdmin,
-	controllers.userController.getAllUser,
-	serializer.userSerializer.getAllUser,
+	userController.getAllUser,
+	userSerializer.getAllUser,
 	genericResponse.sendResponse
 )
 
 router.get(
 	"/refresh-token",
 	authMiddleware.checkRefreshToken,
-	controllers.userController.refreshToken,
+	userController.refreshToken,
 	genericResponse.sendResponse
 )
 
 router.post(
 	"/forget-password",
-	validator.userValidator.forgetPassword,
-	controllers.userController.forgetPassword,
+	userValidator.forgetPassword,
+	userController.forgetPassword,
 	genericResponse.sendResponse
 )
 
 router.post(
 	"/reset-password/:token",
-	validator.userValidator.resetPasswordTokenSchema,
-	validator.userValidator.resetPasswordSchema,
-	controllers.userController.resetPasswordByToken,
+	userValidator.resetPasswordTokenSchema,
+	userValidator.resetPasswordSchema,
+	userController.resetPasswordByToken,
 	genericResponse.sendResponse
 )
 
@@ -67,16 +67,16 @@ router.post(
 	"/reset-user-password",
 	authMiddleware.checkAccessToken,
 	authMiddleware.verifyAdmin,
-	validator.userValidator.adminResetUserPasswordSchema,
-	controllers.userController.adminResetPassword,
+	userValidator.adminResetUserPasswordSchema,
+	userController.adminResetPassword,
 	genericResponse.sendResponse
 )
 
 router.post(
 	"/reset-password",
 	authMiddleware.checkAccessToken,
-	validator.userValidator.resetPasswordSchema,
-	controllers.userController.resetPassword,
+	userValidator.resetPasswordSchema,
+	userController.resetPassword,
 	genericResponse.sendResponse
 )
 
